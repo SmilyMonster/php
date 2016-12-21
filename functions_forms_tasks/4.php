77dde10a8966a5b34d89c37ed3f7c51ed48a81b7
Smily Monster <cudemonster@gmail.com>
2016-12-21 15:54:20 +0200

    Homework N5. Task 4

--- functions_forms_tasks/4.php
+++ functions_forms_tasks/4.php
@@ -1,3 +1,20 @@
 <p>4. Написать функцию, которая выводит список файлов в заданной директории. Директория задается как параметр функции.</p>
 
 <?php
+
+function getFilesList($directory)
+{
+    $fileList = array();
+    if($directory = opendir('.'))
+    {
+        while($entry = readdir($directory))
+        {
+            if(is_file($entry))
+                $fileList[] = $entry;
+        }
+        closedir($directory);
+    }
+    echo '<pre>';
+    print_r($fileList);
+    echo '</pre>';
+}
