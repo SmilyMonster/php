f7bda0583d9925414ea321da9a8622dd3f920287
Smily Monster <cudemonster@gmail.com>
2016-12-21 16:06:04 +0200

    Homework N5. Task 5

--- functions_forms_tasks/5.php
+++ functions_forms_tasks/5.php
@@ -1,3 +1,21 @@
 <p>5. Написать функцию, которая выводит список файлов в заданной директории, которые содержат искомое слово.  Директория и искомое слово задаются как параметры функции.</p>
+<hr />
 
 <?php
+
+    function findWord($directory, $word)
+    {
+        $fileList = array();
+        if($directory = opendir('.'))
+        {
+            while($entry = readdir($directory))
+            {
+                if(strpos($entry, $word) === 0)
+                    $fileList[] = $entry;
+            }
+            closedir($directory);
+        }
+        echo '<pre>';
+        print_r($fileList);
+        echo '</pre>';
+    }
